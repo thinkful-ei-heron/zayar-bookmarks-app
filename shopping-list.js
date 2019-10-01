1a7a2eb634bfe68eca1a3ee2fa415a57ec24e983
zkhin <arandomname@gmail.com>
2019-10-01 12:56:25 -0400

    first commit

---
+++ shopping-list.js
@@ -0,0 +1,129 @@
+import $ from 'jquery';
+
+import store from './store';
+// import item from './item';
+import api from './api';
+
+const generateItemElement = function (item) {
+  let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
+  if (!item.checked) {
+    itemTitle = `
+      <form class="js-edit-item">
+        <input class="shopping-item" type="text" value="${item.name}" />
+      </form>
+    `;
+  }
+
+  return `
+    <li class="js-item-element" data-item-id="${item.id}">
+      ${itemTitle}
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
+};
+
+const generateShoppingItemsString = function (shoppingList) {
+  const items = shoppingList.map((item) => generateItemElement(item));
+  return items.join('');
+};
+
+const render = function () {
+  // Filter item list if store prop is true by item.checked === false
+  // let curErr = store.errors.error;
+  let items = [...store.items];
+  if (store.hideCheckedItems) {
+    items = items.filter(item => !item.checked);
+  }
+
+  // render the shopping list in the DOM
+  const shoppingListItemsString = generateShoppingItemsString(items);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+};
+
+const handleNewItemSubmit = function () {
+  $('#js-shopping-list-form').submit(function (event) {
+    event.preventDefault();
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    api.createItem(newItemName)
+      .then(res => res.json())
+      .then((newItem) => {
+        store.addItem(newItem);
+        render();
+      })
+      .catch(error => console.log(`No name provided`));
+  });
+};
+
+const getItemIdFromElement = function (item) {
+  return $(item)
+    .closest('.js-item-element')
+    .data('item-id');
+};
+
+const handleDeleteItemClicked = function () {
+  // like in `handleItemCheckClicked`, we use event delegation
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    // get the index of the item in store.items
+    const id = getItemIdFromElement(event.currentTarget);
+    // delete the item
+    api.deleteItem(id);
+    store.findAndDelete(id);
+    // render the updated shopping list
+    render();
+  });
+};
+
+const handleEditShoppingItemSubmit = function () {
+  $('.js-shopping-list').on('submit', '.js-edit-item', event => {
+    event.preventDefault();
+    const id = getItemIdFromElement(event.currentTarget);
+    const itemName = $(event.currentTarget).find('.shopping-item').val();
+    api.updateItem(id, { name: itemName })
+      .then(() => {
+        store.findAndUpdate(id, { name: itemName });
+        render();
+      });
+  });
+};
+
+const handleItemCheckClicked = function () {
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
+    const id = getItemIdFromElement(event.currentTarget);
+    let item = store.findById(id);
+    api.updateItem(item.id, { checked: !item.checked })
+      .then(() => {
+        store.findAndUpdate(item.id, { checked: !item.checked });
+        render();
+      });
+    // store.findAndToggleChecked(id);
+  });
+};
+
+const handleToggleFilterClick = function () {
+  $('.js-filter-checked').click(() => {
+    store.toggleCheckedFilter();
+    render();
+  });
+};
+
+const bindEventListeners = function () {
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+  handleEditShoppingItemSubmit();
+  handleToggleFilterClick();
+};
+// This object contains the only exposed methods from this module:
+export default {
+  render,
+  bindEventListeners
+};
---
+++ store.js
@@ -0,0 +1,65 @@
+const store = {
+  bookmarks: [
+    {
+      id: 'x56w',
+      title: 'Title 1',
+      rating: 3,
+      url: 'http://www.title1.com',
+      description: 'lorem ipsum dolor sit',
+      expanded: false
+    },
+    {
+      id: '6ffw',
+      title: 'Title 2',
+      rating: 5,
+      url: 'http://www.title2.com',
+      description: 'dolorum tempore deserunt',
+      expanded: false
+    } 
+    ...
+  ],
+  adding: false,
+  error: null,
+  filter: 0
+};
+
+const items = [];
+let errors = { error: false };
+let hideCheckeditems = false;
+
+const findById = function (id) {
+  return this.items.find(currentItem => currentItem.id === id);
+};
+
+const addItem = function (item) {
+  if (item.name) {
+    console.log(item);
+    this.items.push(item);
+  } else {
+    throw new Error(item.message)
+  }
+};
+
+const findAndUpdate = function (id, newData) {
+  let updatedItem = this.findById(id);
+  Object.assign(updatedItem, newData);
+};
+
+const findAndDelete = function (id) {
+  this.items = this.items.filter(currentItem => currentItem.id !== id);
+};
+
+const toggleCheckedFilter = function () {
+  this.hideCheckedItems = !this.hideCheckedItems;
+};
+
+export default {
+  items,
+  hideCheckeditems,
+  findById,
+  addItem,
+  findAndUpdate,
+  findAndDelete,
+  toggleCheckedFilter,
+  errors
+};
